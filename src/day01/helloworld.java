2f9362042f4eac50940ed9d82827913886920a19
khalid0316 <khalid.mowafi@gmail.com>
2021-04-24 16:43:11 -0400

    added one more line of code

--- src/day01/helloworld.java
+++ src/day01/helloworld.java
@@ -3,6 +3,7 @@ package day01;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("adding GitHub Now");
 
     }
 }
